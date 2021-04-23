ddb01513b3366e4bd9526b76c9664bebdbba8f08
Kent McKinney <kent.p.mckinney@gmail.com>
2021-04-23 16:55:24 -0700

    incremental progress on proof of concept

---
+++ client/public/next.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+    <meta http-equiv="Content-Security-Policy"
+      content="default-src * 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: 'unsafe-inline'; child-src https://www.youtube.com blob: ; worker-src 'self' blob: ; script-src 'self' https://www.youtube.com blob: 'unsafe-inline' 'unsafe-eval' ">
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <meta
+      name="description"
+      content="Web site created using create-react-app"
+    />
+    <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
+    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+    <title>Branch Out Gresham</title>
+  </head>
+  <body>
+    <noscript>Please enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+  </body>
+</html>
